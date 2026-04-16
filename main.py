42743df7ea96de2b1aed99ff7c1c81a7b2fc5dc2
Dartken <amdartken@gmail.com>
2026-04-16 14:40:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import telebot
+import os
+from logic import ImgAPI
+
+API_TOKEN = '8706934648:AAESvVJYM-tweGJIF0KK_-gvoT3hlTx75bA'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def generate_picture(message):
+    api = ImgAPI()
+    promt = message.text
+    result = api.download_image(promt)
+    with open(result, "rb") as photo:
+        bot.send_document(message.chat.id, photo)
+    os.remove(result)
+
+bot.infinity_polling()
